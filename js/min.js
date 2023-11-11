62f397c50163d31c5f3c681d4fa3cfa518d3f113
hajersayeb <145911124+hajersayeb@users.noreply.github.com>
2023-11-11 14:33:22 +0100

    java script
    récupérer les valeurs  et les boutons

--- js/min.js
+++ js/min.js
@@ -1,28 +1,15 @@
-const fullName = document.querySelector('.fullName');
-const dateOfBirth = document.querySelector('.dateOfBirth');
-const gender = document.querySelector('select[name="gender"]');
-const task1 = document.querySelector('.task1');
-const task2 = document.querySelector('.task2');
-const task3 = document.querySelector('.task3');
-
-
-
-
-const result = document.querySelector('.result');
-const submit = document.querySelector('.submit');
-const reset = document.querySelector('.reset');
-
-function validateFullName(fullName {
-        if (!fullName) {
-            return " Full name is required.";
-
-        }
-        if (fullName.length < 3 || fullName.length > 20) {
-            return " Full name must be between 3 and 20 characters long. ";
-        }
-        if (!/^[a-zA-Z ]+$/.test(fullName)) {
-            return " Full name must contain only letters and spaces."
-        }
-        return null;
-
-    }
+//Recuperer les valeurs des champs du formulaire
+const fullName= document.querySelector('.fullName');
+const dateOfBirth= document.querySelector('dateOfBirth');
+const gender= document.querySelector('select[name="gender"]');
+const task1= document.querySelector('.task1');
+const task2= document.querySelector('.task2');
+const task3= document.querySelector('.task3');
+// recuperer les boutons
+const result= document.querySelector('.Result');
+const submit= document.querySelector('.Submit');
+const reset= document.querySelector('.Reset');
+// validation rules fullName
+function validateFullName(fullName){
+    
+}
