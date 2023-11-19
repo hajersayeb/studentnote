85f5d642b6e434dcd6f0c04f101996c4a75fce3c
TasneemDev02 <149237468+MD-youcef@users.noreply.github.com>
2023-11-19 14:47:01 +0100

    input full name
    function validate full name

--- js/min.js
+++ js/min.js
@@ -11,5 +11,37 @@ const submit= document.querySelector('.Submit');
 const reset= document.querySelector('.Reset');
 // validation rules fullName
 function validateFullName(fullName){
-    
+    // Check if the full name is required.
+    if (!fullName){
+        return "Full name is required.";
+    }
+    // Check the length of the full name.
+    if (fullName.length <3 || fullName.length >20){
+        return "Full name must be between 3 and 20 characters long.";
+    }
+    // Check the format of the full name.
+    if (!/^[a-zA-Z]+$/.test(fullName)){
+        return "Full name must contain only letters and spaces.";
+    }
+    // If the full name passes all of the validation rules, return null.
+    return null;
+}
+function validateDate(date){
+    //Check if the date is required.
+    if(!date){
+        return "Date is required.";
+    }
+    // Check the format of the date.
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)){
+        return "Date must be in the format YYYY-MM-DD.";
+    }
+    //Check the range of the date.
+    const today = new Date();
+    const minDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const maxDate = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
+    if(date < minDate || date > maxDate){
+        return "Date must be between today and one year from today.";
+    }
+    // If the date passes all of the validation rules, return null.
+    return null;
 }
